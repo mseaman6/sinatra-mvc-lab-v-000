c3f9363f49d7630a6e83b4b4d243037aeb9d56eb
Monica Seaman <monica.l.seaman@gmail.com>
2018-08-02 12:32:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/' do
+    @piglatinized_text = PigLatinizer.new(params[:user_input])
+
+    erb :results
+  end
+
 end
