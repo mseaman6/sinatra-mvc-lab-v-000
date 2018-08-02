9433284ec050f28fd0c427760fe018ffeac0eb4d
Monica Seaman <monica.l.seaman@gmail.com>
2018-08-02 13:41:26 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,7 +9,7 @@ class PigLatinizer
   end
 
   def piglatin
-    @words_to_alter.collect do |word|
+    @new_text = @words_to_alter.collect do |word|
       if /[aeiou]/.match(word[0])
         word + "way"
       elsif /[^aeiou]/.match(word[0]) && /[^aeiou]/.match(word[1])
